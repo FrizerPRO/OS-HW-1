a7ffb35103f65366b0af7de60b6e0d21fd3a2ad5
FrizerPRO <64581519+FrizerPRO@users.noreply.github.com>
2023-03-19 23:43:03 +0300

    Update prog4.c

--- HW/prog4/prog4.c
+++ HW/prog4/prog4.c
@@ -34,7 +34,8 @@ void reverse_chars(char string[]) {
 const int mes_size = 5000;
 
 int main(int argc, char *argv[]) {
-    char  str_buf[mes_size];
+    char str_buf[mes_size + 1];
+    memset(str_buf, 0, mes_size + 1);
     if(argc != 3){
         printf("./prog.out read.txt write.txt\n");
         return 0;
